085046df1c09fb2b5af9e3198d82bb9f2bd63ae2
Adil0003-alpha <snff0003@gmail.com>
2024-07-14 13:45:56 +0530

    update ingredient

--- src/components/RecipeId.js
+++ src/components/RecipeId.js
@@ -34,84 +34,92 @@ const RecipeId = () => {
           <div className="img">
             <img src={data.strMealThumb} alt="" />
           </div>
-          <div className="content">
-            <button className="btn" onClick={() => setActive("ingredient")}>
-              Ingredient
-            </button>
-            <button className="btn" onClick={() => setActive("instruction")}>
-              Instruction
-            </button>
+          <div className="detailsData">
+            <div className="content">
+              <button className="btn" onClick={() => setActive("ingredient")}>
+                Ingredient
+              </button>
+              <button className="btn" onClick={() => setActive("instruction")}>
+                Instruction
+              </button>
 
-            {active === "ingredient" ? (
-              <div className="ingredient">
-                <div>
-                  <p>
-                    {data.strIngredient1}-{data.strIngredient1}
-                  </p>
-                  <p>
-                    {data.strIngredient2}-{data.strIngredient2}
-                  </p>
-                  <p>
-                    {data.strIngredient3}-{data.strIngredient3}
-                  </p>
-                  <p>
-                    {data.strIngredient4}-{data.strIngredient4}
-                  </p>
-                  <p>
-                    {data.strIngredient5}-{data.strIngredient5}
-                  </p>
-                  <p>
-                    {data.strIngredient6}-{data.strIngredient6}
-                  </p>
-                  <p>
-                    {data.strIngredient7}-{data.strIngredient7}
-                  </p>
-                  <p>
-                    {data.strIngredient8}-{data.strIngredient8}
-                  </p>
-                  <p>
-                    {data.strIngredient9}-{data.strIngredient9}
-                  </p>
-                  <p>
-                    {data.strIngredient10}-{data.strIngredient10}
-                  </p>
+              {active === "ingredient" ? (
+                <div className="ingredient">
+                  <div className="dataitems">
+                    <p>
+                      {data.strIngredient1}-{data.strIngredient1}
+                    </p>
+                    <p>
+                      {data.strIngredient2}-{data.strIngredient2}
+                    </p>
+                    <p>
+                      {data.strIngredient3}-{data.strIngredient3}
+                    </p>
+                    <p>
+                      {data.strIngredient4}-{data.strIngredient4}
+                    </p>
+                    <p>
+                      {data.strIngredient5}-{data.strIngredient5}
+                    </p>
+                  </div>
+                  <div className="data2">
+                    <p>
+                      {data.strIngredient6}-{data.strIngredient6}
+                    </p>
+                    <p>
+                      {data.strIngredient7}-{data.strIngredient7}
+                    </p>
+                    <p>
+                      {data.strIngredient8}-{data.strIngredient8}
+                    </p>
+                    <p>
+                      {data.strIngredient9}-{data.strIngredient9}
+                    </p>
+                    <p>
+                      {data.strIngredient10}-{data.strIngredient10}
+                    </p>
+                  </div>
+                  <div className="dataitems">
+                    <p>
+                      {data.strIngredient11}-{data.strIngredient11}
+                    </p>
+                    <p>
+                      {data.strIngredient12}-{data.strIngredient12}
+                    </p>
+                    <p>
+                      {data.strIngredient13}-{data.strIngredient13}
+                    </p>
+                    <p>
+                      {data.strIngredient14}-{data.strIngredient14}
+                    </p>
+                    <p>
+                      {data.strIngredient15}-{data.strIngredient15}
+                    </p>
+                  </div>
+                  <div className="data2">
+                    <p>
+                      {data.strIngredient16}-{data.strIngredient16}
+                    </p>
+                    <p>
+                      {data.strIngredient17}-{data.strIngredient17}
+                    </p>
+                    <p>
+                      {data.strIngredient18}-{data.strIngredient18}
+                    </p>
+                    <p>
+                      {data.strIngredient19}-{data.strIngredient19}
+                    </p>
+                    <p>
+                      {data.strIngredient20}-{data.strIngredient20}
+                    </p>
+                  </div>
                 </div>
-                <div>
-                  <p>
-                    {data.strIngredient11}-{data.strIngredient11}
-                  </p>
-                  <p>
-                    {data.strIngredient12}-{data.strIngredient12}
-                  </p>
-                  <p>
-                    {data.strIngredient13}-{data.strIngredient13}
-                  </p>
-                  <p>
-                    {data.strIngredient14}-{data.strIngredient14}
-                  </p>
-                  <p>
-                    {data.strIngredient15}-{data.strIngredient15}
-                  </p>
-                  <p>
-                    {data.strIngredient16}-{data.strIngredient16}
-                  </p>
-                  <p>
-                    {data.strIngredient17}-{data.strIngredient17}
-                  </p>
-                  <p>
-                    {data.strIngredient18}-{data.strIngredient18}
-                  </p>
-                  <p>
-                    {data.strIngredient19}-{data.strIngredient19}
-                  </p>
-                  <p>
-                    {data.strIngredient20}-{data.strIngredient20}
-                  </p>
+              ) : (
+                <div className="inst">
+                  <p>{data.strInstructions}</p>
                 </div>
-              </div>
-            ) : (
-              <p id="instruction">{data.strInstructions}</p>
-            )}
+              )}
+            </div>
           </div>
         </div>
       </div>
